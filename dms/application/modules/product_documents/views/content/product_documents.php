9e01a3379e36c70b623678bbbeebfc0802b8c600
Ramakrishna Bodi <ramakrishna@sristibiosciences.com>
2016-01-22 11:18:04 +0530

    cleaned up spaces

--- dms/application/modules/product_documents/views/content/product_documents.php
+++ dms/application/modules/product_documents/views/content/product_documents.php
@@ -9,12 +9,12 @@ $has_records	= isset($records) && is_array($records) && count($records);
     <?php if ($has_records) : ?>
         <table class="table table-condensed">
             <tr class="info">
-                <td colspan="5"><strong>Products Documents</strong></td><td><a class="alert-link" href="content/product_documents/create"><strong>Upload</strong></a></td>    
+                <td colspan="5"><strong>Products Documents</strong></td><td><a class="alert-link" href="content/product_documents/create"><strong>Upload</strong></a></td>
             </tr>
             <tr>
                 <td colspan="7">Lists the status of necessary documentation required against each product. Green indicates that you have uploaded the necessary document</td>
             </tr>
-                <?php 
+                <?php
                     $prod_id = -1;
                     foreach($records as $record) :
                         if($prod_id != $record->product_id) :
@@ -23,26 +23,26 @@ $has_records	= isset($records) && is_array($records) && count($records);
                             echo "<tr><td><span style='width:250px;' class='label label-default'><h5>" . $record->product_name . ' - ' . $record->product_desc . "</h5></td>";
                         endif;
                         
-                        $attachment = NULL;
-
-                        if(isset($docs)){
-                            foreach($docs as $doc){
-                                if($doc->document_id == $record->document_id && $doc->product_id == $record->product_id){
-                                    $attachment = unserialize($doc->attachment);
+                            $attachment = NULL;
+    
+                            if(isset($docs)){
+                                foreach($docs as $doc){
+                                    if($doc->document_id == $record->document_id && $doc->product_id == $record->product_id){
+                                        $attachment = unserialize($doc->attachment);
+                                    }
                                 }
                             }
-                        }
-
-                        if(isset($attachment)) :
-                            echo "<td><a href=" . base_url() . 'uploads/' . $attachment['file_name'] . " target='_blank' ><span class='label label-success'><h5>" . $record->document_name . "</h5></span></td>";
-                        else:
-                            echo "<td><span class='label label-warning'><h5>" . $record->document_name . "</h5></span></td>";
-                        endif;
+    
+                            if(isset($attachment)) :
+                                echo "<td><a href=" . base_url() . 'uploads/' . $attachment['file_name'] . " target='_blank' ><span class='label label-success'><h5>" . $record->document_name . "</h5></span></td>";
+                            else:
+                                echo "<td><span class='label label-warning'><h5>" . $record->document_name . "</h5></span></td>";
+                            endif;
 
                     endforeach;
-                ?>            
+                ?>
         </table>
-    <?php 
+    <?php
         else:
     ?>
         <div class="panel panel-primary">
@@ -52,7 +52,7 @@ $has_records	= isset($records) && is_array($records) && count($records);
             <div class="panel-body">
                 <p>No records found that match your selection. <a href="content/vendor_products/create" class="btn btn-primary" name="Create" id="Create">Create</a> </p>
             </div>
-        </div>    
+        </div>
     <?php
-        endif; 
-    ?> 
+        endif;
+    ?>
